965edc1e52e5f3d3129d5b132ac6b68ede784e5c
BlairWachira <blairkagwi@gmail.com>
2025-06-07 11:35:00 +0300

    Update: made changes to project

--- guess_flag/guess_flag/settings.py
+++ guess_flag/guess_flag/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'guess_flag.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flags',
+        'USER': 'flag_user',
+        'PASSWORD': 'blair900',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
